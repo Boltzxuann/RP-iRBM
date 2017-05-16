b09a077b0f02e514584c1fc9aa94e23e892b567e
Boltzxuann <pengxuan@nudt.edu.cn>
2017-05-16 19:40:11 +0800

    Test

--- evaluation/Test.m
+++ evaluation/Test.m
@@ -1,25 +1,4 @@
-<<<<<<< HEAD
-use_valid=0;
-makebatches_mnist;
-numbatches_valid=size(testbatchdata,3);
-correct = zeros(1,numbatches_valid);
-for tt= 1:numbatches_valid
-    data_val = testbatchdata(:,:,tt);
-    targets_val = testbatchtargets(:,:,tt);
-    p_zy_v_valid = P_yz_v(data_val,J,numclasses,M_hid_visMax,M_hid_yMax,M_hidbiasesMax,M_ybiases,beta,beta0 );
-    PP_y_v_val = sum( p_zy_v_valid ); %%% 1*C*M  %%%
-    P_y_v_val = squeeze(PP_y_v_val);  %%% C*M
-    [P_max,target]=max(P_y_v_val);
-    [one,TstLbls]=max(targets_val');
-    if use_gpu
-        TstLbls=gpuArray(TstLbls);
-    end
-    correct(tt)=sum ( gather(target==TstLbls) );
-    
-    
-end
-TestAccuracy=sum(correct)/10000;
-=======
+
 use_valid=0;
 makebatches_mnist;
 numbatches_valid=size(testbatchdata,3);
@@ -40,5 +19,4 @@ for tt= 1:numbatches_valid
     
 end
 TestAccuracy=sum(correct)/10000;
->>>>>>> origin/master
 fprintf(1, 'Classification error on test set: %6.4f  \n', 1-TestAccuracy);
