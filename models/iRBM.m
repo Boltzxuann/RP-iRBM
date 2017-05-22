f68ce47156c8cd46d336bb6e21726a2d805db2dd
Boltzxuann <pengxuan@nudt.edu.cn>
2017-05-22 13:16:45 +0800

    A minor change

--- models/iRBM.m
+++ models/iRBM.m
@@ -20,7 +20,7 @@ if restart ==1
     CD= 10;  
     
     global_lr = 0.05;
-    Num_inter_initial_lr = 1; %%
+    Num_inter_initial_lr = 0; %%
     initial = Num_inter_initial_lr*ones(1,Maxnumhid);
     start = 1;
     
